83a39d03b8a46688caeca50d1fb93cdb9a534540
Michael E <adebisienads@gmail.com>
2025-09-22 07:17:37 +0100

    Initial Commit

---
+++ Slate/SlateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlateApp.swift
+//  Slate
+//
+//  Created by MICHAEL on 22/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct SlateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SlateTests/SlateTests.swift
@@ -0,0 +1,17 @@
+//
+//  SlateTests.swift
+//  SlateTests
+//
+//  Created by MICHAEL on 22/09/2025.
+//
+
+import Testing
+@testable import Slate
+
+struct SlateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
